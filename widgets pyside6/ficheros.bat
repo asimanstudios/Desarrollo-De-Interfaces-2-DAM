119aa51ee67286c7b794a33cf49a4eb634803cec
Adrián Simarro Manzano <adrisimarroman@gmail.com>
2025-10-17 10:35:33 +0200

    qgroupbpx

---
+++ widgets pyside6/ficheros.bat
@@ -0,0 +1,14 @@
+@echo off
+echo "" > widgets\checkbox.py
+echo "" > widgets\combobox.py
+echo "" > widgets\datetimeedit.py
+echo "" > widgets\dial.py
+echo "" > widgets\groupbox.py
+echo "" > widgets\label.py
+echo "" > widgets\lineedit.py
+echo "" > widgets\pushbutton.py
+echo "" > widgets\radiobuttons.py
+echo "" > widgets\slider.py
+echo "" > widgets\tablewidget.py
+echo "" > widgets\tabwidget.py
+echo "" > widgets\textedit.py
---
+++ widgets pyside6/pyside6_widgets_demo/widgets/__init__.py
@@ -0,0 +1,3 @@
+from . import checkbox, combobox, datetimeedit, dial, groupbox, label, lineedit, pushbutton, radiobuttons, slider, tablewidget, tabwidget, textedit
+
+__all__ = ['checkbox','combobox','datetimeedit','dial','groupbox','label','lineedit','pushbutton','radiobuttons','slider','tablewidget','tabwidget', 'textedit']
